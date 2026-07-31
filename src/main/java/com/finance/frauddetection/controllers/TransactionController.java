f099549d4071a37849d13f5fe46a10f33d4d0ffa
Diksha7876 <diksharana7876@gmail.com>
2026-07-31 06:56:16 +0000

    Changes in Transaction for adding CORS

--- src/main/java/com/finance/frauddetection/controllers/TransactionController.java
+++ src/main/java/com/finance/frauddetection/controllers/TransactionController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin (origins ="*")
 @RequestMapping("/api/transactions")
 public class TransactionController {
     private FraudDetectionService fraudDetectionService;
